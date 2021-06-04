cc248ce08faedd6948a3ac18bccae43b64033846
p-adams <ghusiyy@gmail.com>
2021-06-04 00:12:13 -0700

    remove boilerplate code and init chat box component

---
+++ src/lib/ChatBox.svelte
@@ -0,0 +1,20 @@
+<div class="chat-box">
+    <div>
+        <input/>
+        <button>Send</button>
+    </div>
+</div>
+
+<style>
+    .chat-box {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .chat-box div {
+        border: 1px solid gray;
+        border-radius: 4px;
+        height: 450px;
+        width: 524px;
+    }
+</style>
