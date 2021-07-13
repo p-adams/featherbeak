bcb700edbdb7d73e104c0b11558eab981c8aa7f6
p-adams <ghusiyy@gmail.com>
2021-07-13 10:56:32 -0700

    add FAB hover UI

--- src/lib/ChatFab.svelte
+++ src/lib/ChatFab.svelte
@@ -1,7 +1,7 @@
 <script lang="ts"></script>
 
 <div class="chat-fab-container">
-  <span>🦜</span>
+  <span class="chat-logo">🦜</span>
 </div>
 
 <style>
@@ -9,16 +9,25 @@
     position: absolute;
     right: 0;
     bottom: 0;
+    margin-right: 25px;
     display: flex;
     justify-content: center;
     align-items: center;
     text-align: center;
     height: 100px;
     width: 100px;
-    border-radius: 50px;
+    border-radius: 100%;
     border: 1px solid lightgray;
+    box-shadow: 2px 5px 2px lightgray;
+    cursor: pointer;
   }
-  .chat-fab-container span {
+  .chat-fab-container .chat-logo {
     font-size: 4rem;
+  }
+  .chat-fab-container:hover {
+    height: 110px;
+    width: 110px;
+    border-radius: 100%;
+    border: 2px solid lightblue;
   }
 </style>
