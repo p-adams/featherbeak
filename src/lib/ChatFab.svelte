84929f8ec719161781fa0999667c23d0ede4836e
p-adams <ghusiyy@gmail.com>
2021-07-13 10:16:21 -0700

    remove counter component and center align FAB content

--- src/lib/ChatFab.svelte
+++ src/lib/ChatFab.svelte
@@ -1,6 +1,8 @@
 <script lang="ts"></script>
 
-<div class="chat-fab-container">fab</div>
+<div class="chat-fab-container">
+  <span>🦜</span>
+</div>
 
 <style>
   .chat-fab-container {
@@ -8,10 +10,15 @@
     right: 0;
     bottom: 0;
     display: flex;
+    justify-content: center;
     align-items: center;
+    text-align: center;
     height: 100px;
     width: 100px;
     border-radius: 50px;
     border: 1px solid lightgray;
+  }
+  .chat-fab-container span {
+    font-size: 4rem;
   }
 </style>
