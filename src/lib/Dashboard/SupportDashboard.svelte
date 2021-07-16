eeaeb44e4f4a11a55dcf8b0b87dfc2f0d4839746
p-adams <ghusiyy@gmail.com>
2021-07-16 15:23:15 -0700

    dashboard outline

--- src/lib/Dashboard/SupportDashboard.svelte
+++ src/lib/Dashboard/SupportDashboard.svelte
@@ -1,6 +1,11 @@
 <script>
 </script>
 
-<section>Support Dashboard</section>
+<div>
+  <h4>Support Dashboard</h4>
+  <div class="insights-container" />
+  <div class="chat-list" />
+  <div class="chat-box" />
+</div>
 
 <style></style>
